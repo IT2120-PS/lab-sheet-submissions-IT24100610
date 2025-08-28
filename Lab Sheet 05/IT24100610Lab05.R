d93f8cda98cb80b558588524257b0002530933b3
IT24100610 <it24100610@my.sliit.lk>
2025-08-28 14:27:39 +0530

    IT24100610Lab05.R

---
+++ Lab Sheet 05/IT24100610Lab05.R
@@ -0,0 +1,44 @@
+setwd("C:\\Users\\IT24100610\\Desktop\\IT24100610Lab05")
+getwd()
+#Q1
+Delivery_Times <- read.table("data.txt", header = TRUE,sep = ",")
+fix(Delivery_Times)
+attach(Delivery_Times)
+
+#Q2
+names(Delivery_Times)<-c("D_Time")
+attach(Delivery_Times)
+fix(Delivery_Times)
+histogram<-hist (D_Time,main="Histogram for deliver times", breaks = seq(20, 70, length = 10), right = FALSE)
+
+#Q3
+Most of the data is at the low end, forming a peak on the left, and a few high values stretch the graph out to the right.
+
+#Q4
+breaks <- round (histogram$breaks)
+freq <- histogram$counts
+mids <- histogram$mids
+
+classes <- c()
+
+##Creating  "for" loop to assign classes of the frequency distribution into "Classes" variable
+for(i in 1:length(breaks)-1){
+  Classes[i] <- paste0("[", breaks[i],",",breaks[i+1],")")
+}
+
+cum.freq <- cumsum(freq)
+
+new <- c()
+
+for(i in 1: length(breaks)){
+  if(i==1){
+    new[i]= 0
+  } else {
+    new[i] = cum.freq[i-1]
+  }
+}
+
+
+plot (breaks, new, type = '7', main = "Cumulative Frequency Polygon for deliver times", xlab = "Shareholders", ylab = "Cumulative Frequency", yim = cCO, max (cum. freq)))|
+  
+  
